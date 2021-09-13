9f4e3b9a21bede8712564ebd9d8a958f96eeda1c
kylejb <47502769+kylejb@users.noreply.github.com>
2021-09-13 00:30:56 -0400

    feat: add webpack rules for asset filetypes

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/inline',
+            },
         ],
     },
     output: {
